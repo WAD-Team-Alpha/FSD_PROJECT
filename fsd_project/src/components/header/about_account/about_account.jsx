d3b3e81027a52306879d831c8ff9fca0b83db057
surya0180 <teja8551@gmail.com>
2021-11-01 19:01:47 +0530

    Added signin Button

--- fsd_project/src/components/header/about_account/about_account.jsx
+++ fsd_project/src/components/header/about_account/about_account.jsx
@@ -3,9 +3,15 @@ import classes from './about_account.module.css';
 
 const AboutAccount = () => {
     const [classname, setClassname] = useState(classes.hide);
+    const [signin, setSignin] = useState(false);
 
     function showAccountOptions() {
         classname == classes.hide ? setClassname(classes.show) : setClassname(classes.hide)
+    }
+
+    function verifySignin() {
+        signin ? setSignin(false) : setSignin(true)
+        setClassname(classes.hide)
     }
     
     return <React.Fragment>
@@ -13,14 +19,15 @@ const AboutAccount = () => {
             <a className={classes.aboutbtn}>About</a>
         </div>
         <div className="col-md-1 col-sm-1">
-            <div className={classes.account_dropdown}>
+            { !signin && <a className={classes.signinbtn} onClick={verifySignin}>Signin</a> }
+            { signin && <div className={classes.account_dropdown}>
                 <button onClick={showAccountOptions} className={classes.dropdown_button}>pici</button>
                 <div id={classname} className={classes.account_options}>
-                    <a href="#home">Signout</a>
-                    <a href="#about">Account</a>
-                    <a href="#contact">Settings</a>
+                    <a onClick={verifySignin}>Signout</a>
+                    <a >Account</a>
+                    <a >Settings</a>
                 </div>
-            </div>
+            </div> }
         </div>
     </React.Fragment>
 }
