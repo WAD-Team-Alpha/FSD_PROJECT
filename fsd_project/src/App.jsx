0bc4c6e5b081a61eceec275c9a3db91cb2aeb8c5
bannu0snake <mahaboob.developer@gmail.com>
2021-11-02 22:07:54 +0530

    linked signin button and sign page

--- fsd_project/src/App.jsx
+++ fsd_project/src/App.jsx
@@ -1,15 +1,21 @@
-import Header from './components/header/header';
-import Main from './components/main/main';
-import Footer from './components/footer/footer';
+import Header from "./components/header/header";
+import Main from "./components/main/main";
+import Footer from "./components/footer/footer";
 import React, { useState } from "react";
 import Signup from "./components/authentication/Signup";
 
 const App = () => {
   const [signup, setSignup] = useState(false);
-  const [signin, setSignin] = useState(false);
+  // const [signin, setSignin] = useState(false);
+  const [signedin, setSignedin] = useState(true);
+  const loginButtonHandler = (state) => {
+    setSignup(state);
+    setSignedin(true);
+    console.log("login handler");
+  };
   return (
     <div>
-      <Header />
+      <Header loginButtonHandler={loginButtonHandler} signin={signedin} />
       {signup && <Signup />}
       {/* {signin &&<Signin />} */}
       <Main />
