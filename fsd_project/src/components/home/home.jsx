32dda19f0360229971a2a8e095d254b325cbebb8
surya0180 <teja8551@gmail.com>
2021-12-08 15:13:03 +0530

    Completed main page

--- fsd_project/src/components/home/home.jsx
+++ fsd_project/src/components/home/home.jsx
@@ -10,8 +10,57 @@ const Home = () => {
             <Slider></Slider>
             <Box sx={{ height: '2em' }} />
             <Container maxWidth="xl">
+
                 <Box sx={{display: 'flex'}}>
                     <Typography variant="h5" sx={{ fw: '900', paddingBottom: '.5em', flexGrow: '1' }}>Trending</Typography>
+                    <Link component="button" sx={{ color: "#6e71f8" }} underline="none">View more</Link>
+                </Box>
+                <Divider />
+                <Content />
+                <Box sx={{ height: '5em' }} />
+
+                <Box sx={{display: 'flex'}}>
+                    <Typography variant="h5" sx={{ fw: '900', paddingBottom: '.5em', flexGrow: '1' }}>Recently Viewed</Typography>
+                    <Link component="button" sx={{ color: "#6e71f8" }} underline="none">View more</Link>
+                </Box>
+                <Divider />
+                <Content />
+                <Box sx={{ height: '5em' }} />
+
+                <Box sx={{display: 'flex'}}>
+                    <Typography variant="h5" sx={{ fw: '900', paddingBottom: '.5em', flexGrow: '1' }}>Electronics</Typography>
+                    <Link component="button" sx={{ color: "#6e71f8" }} underline="none">View more</Link>
+                </Box>
+                <Divider />
+                <Content />
+                <Box sx={{ height: '5em' }} />
+
+                <Box sx={{display: 'flex'}}>
+                    <Typography variant="h5" sx={{ fw: '900', paddingBottom: '.5em', flexGrow: '1' }}>Sports</Typography>
+                    <Link component="button" sx={{ color: "#6e71f8" }} underline="none">View more</Link>
+                </Box>
+                <Divider />
+                <Content />
+                <Box sx={{ height: '5em' }} />
+
+                <Box sx={{display: 'flex'}}>
+                    <Typography variant="h5" sx={{ fw: '900', paddingBottom: '.5em', flexGrow: '1' }}>Clothes</Typography>
+                    <Link component="button" sx={{ color: "#6e71f8" }} underline="none">View more</Link>
+                </Box>
+                <Divider />
+                <Content />
+                <Box sx={{ height: '5em' }} />
+
+                <Box sx={{display: 'flex'}}>
+                    <Typography variant="h5" sx={{ fw: '900', paddingBottom: '.5em', flexGrow: '1' }}>Entertainment</Typography>
+                    <Link component="button" sx={{ color: "#6e71f8" }} underline="none">View more</Link>
+                </Box>
+                <Divider />
+                <Content />
+                <Box sx={{ height: '5em' }} />
+                
+                <Box sx={{display: 'flex'}}>
+                    <Typography variant="h5" sx={{ fw: '900', paddingBottom: '.5em', flexGrow: '1' }}>Movies</Typography>
                     <Link component="button" sx={{ color: "#6e71f8" }} underline="none">View more</Link>
                 </Box>
                 <Divider />
